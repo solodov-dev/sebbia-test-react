b3bb9614aea800fc2205600cbbf56b0d344f0e80
Andrey Solodov <solodov.dev@gmail.com>
2020-02-20 08:08:08 +0300

    Updated Details.js to Hooks

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,36 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class Details extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      article: {},
-    };
-  }
+function Details(props) {
+  const [article,setArticle] = useState(props.location.state);
 
-  componentDidMount() {
-    this.setState({ article: this.props.location.state });
+  useEffect(() => {
     axios
       .get(
-        `http://testtask.sebbia.com/v1/news/details?id=${this.props.location.state.id}`
+        `http://testtask.sebbia.com/v1/news/details?id=${props.location.state.id}`
       )
-      .then(res => this.setState({ article: res.data.news }))
+      .then(res => setArticle(res.data.news ))
       .catch(err => console.log(err));
-  }
+  })
 
-  render() {
     return (
       <div>
-        <h1>{this.state.article.title}</h1>
-        <h2>{this.state.article.shortDescription}</h2>
+        <h1>{article.title}</h1>
+        <h2>{article.shortDescription}</h2>
         <p
           dangerouslySetInnerHTML={{
-            __html: this.state.article.fullDescription,
+            __html: article.fullDescription,
           }}
         ></p>
       </div>
     );
   }
-}
+
 export default Details;
